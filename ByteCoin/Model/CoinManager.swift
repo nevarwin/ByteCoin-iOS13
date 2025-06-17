64a9cb9bd37fe3222b08d2953e11392f96a75ad0
nevarwin <ravencsolis@gmail.com>
2025-06-17 09:45:43 +0800

    some done

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,40 @@
 
 import Foundation
 
+protocol CoinManagerDelegate{
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
+    var delegate: CoinManagerDelegate?
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "0ee3ab2e-bd6a-434d-b957-a66f2cffb9a9"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    func getCoinPrice(for currency: String){
+        let url = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        performRequest(with: url)
+        
+    }
+    
+    func performRequest(with urlString: String){
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+            }
+            
+//            if let safeData = data {
+//                if let rate = self.parseJSON(){
+//                    self.delegate?.didUpdateRate(self,)
+//                }
+//            }
+        }
+    }
 }
